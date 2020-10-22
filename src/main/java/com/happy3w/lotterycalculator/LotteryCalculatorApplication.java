ed73edf91bde75985f1e8c6b8a68aee0e7b9f2bb
Yushan Gao <ysgao@thoughtworks.com>
2020-10-22 21:29:32 +0800

    chore: init project

---
+++ src/main/java/com/happy3w/lotterycalculator/LotteryCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.happy3w.lotterycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happy3w/lotterycalculator/LotteryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy3w.lotterycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
