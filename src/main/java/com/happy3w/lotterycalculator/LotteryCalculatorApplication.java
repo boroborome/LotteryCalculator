64f353002e9e35dbacee92066737eec6f8ad932d
Yushan Gao <ysgao@thoughtworks.com>
2021-03-04 23:48:55 +0800

    fix bug

--- src/main/java/com/happy3w/lotterycalculator/LotteryCalculatorApplication.java
+++ src/main/java/com/happy3w/lotterycalculator/LotteryCalculatorApplication.java
@@ -19,126 +19,137 @@ import java.util.Map;
 @SpringBootApplication
 public class LotteryCalculatorApplication {
 
-	private static final List<RememberFunInfo> RememberFuns = Arrays.asList(
-			new RememberFunInfo("power weight", (weight) -> {
-				weight += 1;
-				weight *= weight;
-				return weight;
-			}),
-			new RememberFunInfo("plus 1", (weight) -> {
-				weight += 1;
-				return weight;
-			})
-	);
+    private static final List<RememberFunInfo> RememberFuns = Arrays.asList(
+            new RememberFunInfo("power weight", (weight) -> {
+                weight += 1;
+                weight *= weight;
+                return weight;
+            }),
+            new RememberFunInfo("plus 1", (weight) -> {
+                weight += 1;
+                return weight;
+            })
+    );
 
-	public static void main(String[] args) throws IOException {
-		List<LotteryInfo> lotteryInfos = loadAllLotteryInfos();
-		List<IBestScoreComparator> scoreComparators = Arrays.asList(
-				new MoneyComparator(),
-				new WinCountComparator()
-		);
-		for (IBestScoreComparator scoreComparator : scoreComparators) {
-			simulate(lotteryInfos, scoreComparator, 4);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        List<LotteryInfo> lotteryInfos = loadAllLotteryInfos();
+        List<IBestScoreComparator> scoreComparators = Arrays.asList(
+                new MoneyComparator(),
+                new WinCountComparator()
+        );
+        for (IBestScoreComparator scoreComparator : scoreComparators) {
+            simulate(lotteryInfos, scoreComparator, 3);
+        }
+    }
 
-	private static void simulate(List<LotteryInfo> lotteryInfos, IBestScoreComparator scoreComparator, int times) {
-		TestScore bestScore = iterativeCalculateBestScore(lotteryInfos, scoreComparator, times);
+    private static void simulate(List<LotteryInfo> lotteryInfos, IBestScoreComparator scoreComparator, int times) {
+        TestScore bestScore = iterativeCalculateBestScore(lotteryInfos, scoreComparator, times);
 
-		int cost = lotteryInfos.size() * 2;
-		System.out.println(bestScore.getWinCounts());
-		System.out.println(MessageFormat.format("RemFun:{0};ForgetRate:{1}; Cost:{2}; Win:{3}",
-				bestScore.getRememberFunInfo().getName(),
-				bestScore.getForgetRate(),
-				cost,
-				bestScore.getWinMoney()));
-		System.out.println(bestScore.getNextLottery().getDesc());
-	}
+        int cost = lotteryInfos.size() * 2;
+        System.out.println(bestScore.getWinCounts());
+        System.out.println(MessageFormat.format("RemFun:{0};ForgetRate:{1}; Cost:{2}; Win:{3}",
+                bestScore.getRememberFunInfo().getName(),
+                bestScore.getForgetRate(),
+                cost,
+                bestScore.getWinMoney()));
+        System.out.println(bestScore.getNextLottery().getDesc());
+    }
 
-	private static final int StepCount = 100;
+    private static final int StepCount = 1000;
 
-	private static TestScore iterativeCalculateBestScore(List<LotteryInfo> lotteryInfos, IBestScoreComparator scoreComparator, int times) {
-		double start = 0;
-		double end = 1;
-		TestScore bestScore = null;
-		for (int i = 0; i < times; i++) {
-			TestScore curScore = calculateBestScore(lotteryInfos, start, end);
-			if (bestScore == null) {
-				bestScore = curScore;
-			} else {
-				bestScore = scoreComparator.bestScore(curScore, bestScore);
-			}
+    private static TestScore iterativeCalculateBestScore(List<LotteryInfo> lotteryInfos, IBestScoreComparator scoreComparator, int times) {
+        double start = 0;
+        double end = 1;
+        TestScore bestScore = null;
+        for (int i = 0; i < times; i++) {
+            TestScore curScore = calculateBestScore(lotteryInfos, start, end, scoreComparator);
+            if (bestScore == null) {
+                bestScore = curScore;
+            } else {
+                bestScore = scoreComparator.bestScore(curScore, bestScore);
+            }
 
-			double step = (end - start) / StepCount;
-			double newForgetRate = curScore.getForgetRate();
-			start = newForgetRate - step;
-			end = newForgetRate + step;
-		}
-		return bestScore;
-	}
+            double step = (end - start) / StepCount;
+            double newForgetRate = curScore.getForgetRate();
+            start = newForgetRate - step;
+            end = newForgetRate + step;
+        }
+        return bestScore;
+    }
 
-	private static TestScore calculateBestScore(List<LotteryInfo> lotteryInfos, double start, double end) {
-		TestScore bestScore = null;
-		double step = (end - start) / StepCount;
-		for (double forgetRate = start + step; forgetRate < end; forgetRate += step) {
-			for (RememberFunInfo funInfo : RememberFuns) {
-				bestScore = calculateBestScore(funInfo, forgetRate, lotteryInfos, bestScore);
-			}
-		}
-		return bestScore;
-	}
+    private static TestScore calculateBestScore(List<LotteryInfo> lotteryInfos,
+                                                double start,
+                                                double end,
+                                                IBestScoreComparator scoreComparator) {
+        TestScore bestScore = null;
+        double step = (end - start) / StepCount;
+        for (double forgetRate = start + step; forgetRate < end; forgetRate += step) {
+            for (RememberFunInfo funInfo : RememberFuns) {
+                bestScore = calculateBestScore(funInfo, forgetRate, lotteryInfos, bestScore, scoreComparator);
+            }
+        }
+        return bestScore;
+    }
 
-	private static TestScore calculateBestScore(RememberFunInfo funInfo, double forgetRate, List<LotteryInfo> lotteryInfos, TestScore bestScore) {
-		LotteryCalculator lotteryCalculator = new LotteryCalculator(funInfo.getRememberFun(), forgetRate);
-		TestResult testResult = testCalculator(lotteryCalculator, lotteryInfos);
-		Map<String, Integer> winCounts = testResult.getWinCounts();
-		int winMoney = calculateMoney(winCounts);
-		if (bestScore == null || bestScore.getWinMoney() < winMoney) {
-			bestScore = TestScore.builder()
-					.rememberFunInfo(funInfo)
-					.forgetRate(forgetRate)
-					.winCounts(winCounts)
-					.winMoney(winMoney)
-					.nextLottery(testResult.nextLottery)
-					.build();
-		}
-		return bestScore;
-	}
+    private static TestScore calculateBestScore(RememberFunInfo funInfo,
+                                                double forgetRate,
+                                                List<LotteryInfo> lotteryInfos,
+                                                TestScore bestScore,
+                                                IBestScoreComparator scoreComparator) {
+        LotteryCalculator lotteryCalculator = new LotteryCalculator(funInfo.getRememberFun(), forgetRate);
+        TestResult testResult = testCalculator(lotteryCalculator, lotteryInfos);
+        Map<String, Integer> winCounts = testResult.getWinCounts();
+        int winMoney = calculateMoney(winCounts);
 
-	private static TestResult testCalculator(LotteryCalculator lotteryCalculator, List<LotteryInfo> lotteryInfos) {
-		Map<String, Integer> winCounts = new HashMap<>();
-		LotteryInfo nextLottery = null;
-		for (LotteryInfo info : lotteryInfos) {
+        TestScore curScore = TestScore.builder()
+                .rememberFunInfo(funInfo)
+                .forgetRate(forgetRate)
+                .winCounts(winCounts)
+                .winMoney(winMoney)
+                .nextLottery(testResult.nextLottery)
+                .build();
+        if (bestScore == null) {
+            bestScore = curScore;
+        } else {
+            bestScore = scoreComparator.bestScore(bestScore, curScore);
+        }
+        return bestScore;
+    }
 
-			LotteryInfo newLottery = lotteryCalculator.accept(info);
-			if (nextLottery != null) {
-				String winPrice = nextLottery.winPrice(info);
-				int count = winCounts.getOrDefault(winPrice, 0);
-				winCounts.put(winPrice, count + 1);
-			}
-			nextLottery = newLottery;
-		}
-		return new TestResult(winCounts, nextLottery);
-	}
+    private static TestResult testCalculator(LotteryCalculator lotteryCalculator, List<LotteryInfo> lotteryInfos) {
+        Map<String, Integer> winCounts = new HashMap<>();
+        LotteryInfo nextLottery = null;
+        for (LotteryInfo info : lotteryInfos) {
 
-	private static List<LotteryInfo> loadAllLotteryInfos() throws IOException {
-		InputStream inputStream = LotteryCalculatorApplication.class.getResourceAsStream("/lottery-history.txt");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            LotteryInfo newLottery = lotteryCalculator.accept(info);
+            if (nextLottery != null) {
+                String winPrice = nextLottery.winPrice(info);
+                int count = winCounts.getOrDefault(winPrice, 0);
+                winCounts.put(winPrice, count + 1);
+            }
+            nextLottery = newLottery;
+        }
+        return new TestResult(winCounts, nextLottery);
+    }
 
-		List<LotteryInfo> infos = new ArrayList<>();
-		for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-			LotteryInfo info = LotteryInfo.decode(line);
-			infos.add(info);
-		}
-		return infos;
-	}
+    private static List<LotteryInfo> loadAllLotteryInfos() throws IOException {
+        InputStream inputStream = LotteryCalculatorApplication.class.getResourceAsStream("/lottery-history.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-	private static int calculateMoney(Map<String, Integer> winCounts) {
-		int money = 0;
-		for (Map.Entry<String, Integer> entry : winCounts.entrySet()) {
-			money += (entry.getValue() * LotteryInfo.winMoney(entry.getKey()));
-		}
-		return money;
-	}
+        List<LotteryInfo> infos = new ArrayList<>();
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            LotteryInfo info = LotteryInfo.decode(line);
+            infos.add(info);
+        }
+        return infos;
+    }
+
+    private static int calculateMoney(Map<String, Integer> winCounts) {
+        int money = 0;
+        for (Map.Entry<String, Integer> entry : winCounts.entrySet()) {
+            money += (entry.getValue() * LotteryInfo.winMoney(entry.getKey()));
+        }
+        return money;
+    }
 
 }
